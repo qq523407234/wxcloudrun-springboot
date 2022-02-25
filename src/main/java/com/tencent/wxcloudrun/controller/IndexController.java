3ad654e10d784fc68007e2c8fb71c41636102b88
yaodongliu <yaodongliu@88.com>
2022-02-25 15:38:48 +0800

    :ok_hand: 微信验证

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,22 +1,34 @@
 package com.tencent.wxcloudrun.controller;
 
+import com.tencent.wxcloudrun.aes.AesException;
+import com.tencent.wxcloudrun.aes.WXBizMsgCrypt;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * index控制器
  */
 @Controller
-
 public class IndexController {
 
-  /**
-   * 主页页面
-   * @return API response html
-   */
-  @GetMapping
-  public String index() {
-    return "index";
-  }
+    @Value("${wx.token}")
+    private String token;
+    @Value("${wx.encodingAesKey}")
+    private String encodingAesKey;
+    @Value("${wx.appId}")
+    private String appId;
+
+    /**
+     * 主页页面
+     * @return API response html
+     */
+    @GetMapping
+    public String index(@RequestParam("msgSignature") String msgSignature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr)
+            throws AesException {
+        WXBizMsgCrypt wxBizMsgCrypt = new WXBizMsgCrypt(token, encodingAesKey, appId);
+        return wxBizMsgCrypt.verifyUrl(msgSignature, timestamp, nonce, echostr);
+    }
 
 }
