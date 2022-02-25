f48e0ec240ec0b6aadf888b02f38c0f6552bef96
yaodongliu <yaodongliu@88.com>
2022-02-25 16:32:44 +0800

    :ok_hand: update

--- src/main/java/com/tencent/wxcloudrun/controller/WxController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/WxController.java
@@ -5,6 +5,7 @@ import com.tencent.wxcloudrun.aes.WXBizMsgCrypt;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +13,8 @@ import org.springframework.web.bind.annotation.RestController;
  * wx控制器
  */
 @Slf4j
-@RestController("/wx")
+@RestController
+@RequestMapping("/wx")
 public class WxController {
 
     @Value("${wx.token}")
