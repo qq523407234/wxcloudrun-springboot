fc60d56e8bc6ca91d1e34835f56c7de57b6e1ea6
yaodongliu <yaodongliu@88.com>
2022-02-25 16:20:02 +0800

    :ok_hand: update

--- src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
+++ src/main/java/com/tencent/wxcloudrun/controller/IndexController.java
@@ -1,34 +1,22 @@
 package com.tencent.wxcloudrun.controller;
 
-import com.tencent.wxcloudrun.aes.AesException;
-import com.tencent.wxcloudrun.aes.WXBizMsgCrypt;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * index控制器
  */
 @Controller
-public class IndexController {
 
-    @Value("${wx.token}")
-    private String token;
-    @Value("${wx.encodingAesKey}")
-    private String encodingAesKey;
-    @Value("${wx.appId}")
-    private String appId;
+public class IndexController {
 
     /**
      * 主页页面
      * @return API response html
      */
     @GetMapping
-    public String index(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr)
-            throws AesException {
-        WXBizMsgCrypt wxBizMsgCrypt = new WXBizMsgCrypt(token, encodingAesKey, appId);
-        return wxBizMsgCrypt.verifyUrl(signature, timestamp, nonce, echostr);
+    public String index() {
+        return "index";
     }
 
 }
---
+++ src/main/java/com/tencent/wxcloudrun/controller/WxController.java
@@ -0,0 +1,39 @@
+package com.tencent.wxcloudrun.controller;
+
+import com.tencent.wxcloudrun.aes.AesException;
+import com.tencent.wxcloudrun.aes.WXBizMsgCrypt;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * wx控制器
+ */
+@Slf4j
+@RestController("/wx")
+public class WxController {
+
+    @Value("${wx.token}")
+    private String token;
+    @Value("${wx.encodingAesKey}")
+    private String encodingAesKey;
+    @Value("${wx.appId}")
+    private String appId;
+
+    @GetMapping("/verifyUrl")
+    public String verifyUrl(@RequestParam("signature") String signature, @RequestParam("timestamp") String timestamp, @RequestParam("nonce") String nonce, @RequestParam("echostr") String echostr)
+            throws AesException {
+        log.info("token: {}", token);
+        log.info("encodingAesKey: {}", encodingAesKey);
+        log.info("appId: {}", appId);
+        WXBizMsgCrypt wxBizMsgCrypt = new WXBizMsgCrypt(token, encodingAesKey, appId);
+        log.info("signature: {}", signature);
+        log.info("timestamp: {}", timestamp);
+        log.info("nonce: {}", nonce);
+        log.info("echostr: {}", echostr);
+        return wxBizMsgCrypt.verifyUrl(signature, timestamp, nonce, echostr);
+    }
+
+}
